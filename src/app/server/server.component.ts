54014be57516e8a5a83cdc797d467e26e50d4530
Phat Duong <beebapcay@gmail.com>
2022-02-21 09:41:02 +0700

    Using ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
+  static STATUS: string[] = ['online', 'offline'];
+
   id: number = 127001;
   status: string = 'offline';
 
+  constructor() {
+    this.status = ServerComponent.STATUS[Math.round(Math.random())];
+  }
+
   getStatus() {
     return this.status;
+  }
+
+  getColor() {
+    return this.status === 'online' ? 'green' : 'red';
   }
 }
